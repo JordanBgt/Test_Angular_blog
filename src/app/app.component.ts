dd0e0463b3530b3e27d78e5eb0ac49cee16fefa2
Bgt <jordanbouget@gmail.com>
2019-10-14 15:45:28 +0200

    App component script
    It contains title and posts array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from './Post';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog-application';
+
+  posts = [
+    new Post('My first post','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae enim libero. Aliquam tincidunt quis tellus scelerisque pretium. Vestibulum venenatis in nisl id lobortis. Nullam volutpat ultrices quam a hendrerit. Duis accumsan libero et aliquet iaculis.', 25),
+    new Post('My second post', 'Sed et convallis elit. Vestibulum sed interdum est, at maximus mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.', -10),
+    new Post('My third post', 'Phasellus cursus, diam varius feugiat auctor, tellus sapien faucibus eros, ac vestibulum ligula lectus a nulla.', 0)
+  ];
 }
