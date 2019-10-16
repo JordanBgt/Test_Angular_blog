578f09c955bac7acbe6e9ca282e2f2679791a99c
jordan <jordanbouget@gmail.com>
2019-10-16 15:50:20 +0200

    Add new components, define routes, imports modules, add PostService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { NewPostComponent } from './new-post/new-post.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import {PostsService} from './services/posts.service';
+
+const appRoutes: Routes = [
+  { path: 'posts', component: PostListComponent },
+  { path: 'posts/view/:id', component: PostListItemComponent },
+  { path: 'new', component: NewPostComponent },
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: '**', redirectTo: 'posts' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostListItemComponent
+    PostListItemComponent,
+    NewPostComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    PostsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
