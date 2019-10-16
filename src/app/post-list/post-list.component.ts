c6e07d4bf6f45d95828172f08d62c4cc09711f06
jordan <jordanbouget@gmail.com>
2019-10-16 15:56:25 +0200

    Update post-list-component : it contains the list of posts provide by PostsService

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,16 +1,53 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Post } from '../Post';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Post } from '../models/Post';
+import {Subscription} from 'rxjs';
+import {PostsService} from '../services/posts.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent implements OnInit {
-  @Input() posts: Array<Post>;
-  constructor() { }
+export class PostListComponent implements OnInit , OnDestroy {
+
+  posts: Post[];
+  postsSubscription: Subscription;
+
+  constructor(private postsService: PostsService, private router: Router) { }
 
   ngOnInit() {
+    this.postsSubscription = this.postsService.postsSubject.subscribe(
+      (posts: Post[]) => {
+        this.posts = posts;
+      }
+    );
+    this.postsService.getPosts();
+    this.postsService.emitPosts();
   }
 
+  onDeletePost(post: Post) {
+    this.postsService.removePost(post);
+  }
+
+  onIncrementLoveIts(post: Post) {
+    post.loveIts++;
+    console.log(post);
+    this.postsService.savePosts();
+    this.postsService.emitPosts();
+  }
+
+  onDecrementLoveIts(post: Post) {
+    post.loveIts--;
+    this.postsService.savePosts();
+    this.postsService.emitPosts();
+  }
+
+  onPostView(id: number) {
+    this.router.navigate(['/posts', 'view', id]);
+  }
+
+  ngOnDestroy() {
+    this.postsSubscription.unsubscribe();
+  }
 }
